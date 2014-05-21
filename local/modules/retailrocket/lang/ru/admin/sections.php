69ed27bd5d677cc6dbad46e6ba3e56ebd26dcb93
Zhiltsov Dmitry <info@zhiltsov.info>
2014-05-22 02:02:59 +0600

    Clean

--- local/modules/retailrocket/lang/ru/admin/sections.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$MESS['DEMO_TABLE_HEADER_NAME'] = 'Название группы';
-$MESS['DEMO_PROMPT'] = 'Выберите группы и задайте значение наценки';
-$MESS['DEMO_ERROR_NOT_SET_IBLOCK'] = 'В настройках модуля не задан торговый каталог.';
-$MESS['DEMO_ERROR_NOT_SET_MARKUP'] = 'Список наценок пуст.';
